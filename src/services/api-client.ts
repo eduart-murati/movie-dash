904e8382cee8d425b390d7b632105387c8fdf15b
Eduart Murati <edihes@gmail.com>
2025-09-19 13:10:47 +0200

    Movie Star Rating

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL:'https://api.themoviedb.org/3/discover/',
-    headers: {   
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZTVmOGE2ZDI5NWI4ZmVhZWQ4YWE1NmI5YzcwYWFlYyIsIm5iZiI6MTc1NzU5OTIyNy41OTYsInN1YiI6IjY4YzJkNWZiNGIwZTgxNzkyODhkMDcwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5o2aGW5PH7pQ4I-adVqKY9YCiVyb0ZxMEsJr3ReJJgs' 
-  },
-})
+
 
 // export default axios.create({
 //     baseURL:'https://api.themoviedb.org/3/discover/',
