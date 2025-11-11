b71f545e7e4ed714ebbbcb1635566f507401b64c
Eduart Murati <edihes@gmail.com>
2025-11-11 16:31:17 +0100

    Warnings final

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,12 +7,17 @@ export default axios.create({
   },    
 })
 
-// export default axios.create({
-//     baseURL:'https://api.themoviedb.org/3/',
-//     headers: {   
-//         Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}` 
-//   },    
-// })
+// const apiClient = axios.create({
+//   baseURL: "https://api.themoviedb.org/3/",
+//   headers: {
+//     // Edited
+//     Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+//   },
+// });
+
+// export default apiClient;
+
+
 
 // export default axios.create({
 //     baseURL:'https://api.themoviedb.org/3/discover/',
