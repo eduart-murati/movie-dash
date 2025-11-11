2c3c3fa51093a77132b91ab97c86a01fa2093c3d
Eduart Murati <edihes@gmail.com>
2025-11-11 15:56:33 +0100

    warnings

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const apiClient = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  headers: {
-    // Edited
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-  },
-});
+export default axios.create({
+    baseURL:'https://api.themoviedb.org/3/',
+    headers: {   
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}` 
+  },    
+})
 
-export default apiClient;
+// export default axios.create({
+//     baseURL:'https://api.themoviedb.org/3/discover/',
+//     headers: { 
+//         Authorization: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
+//   },
+// })
